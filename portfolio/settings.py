89f5af38acf21b7539751ca5ee941945e96c758e
Avishek Das <davishek7@gmail.com>
2021-07-08 00:54:19 +0530

    changed database url

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -1,5 +1,7 @@
-from pathlib import Path
 import os
+from pathlib import Path
+import dj_database_url
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -57,12 +59,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'portfolio.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 AUTH_PASSWORD_VALIDATORS = [
